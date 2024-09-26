3cd46aa20464835178e5717847f53cb97ba28f41
CLMNTDFR <deferclement59@gmail.com>
2024-09-26 10:53:02 +0200

    Implement pop-up information window for leftnavbar elements

--- client/src/components/LeftNav.js
+++ client/src/components/LeftNav.js
@@ -1,31 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const LeftNav = () => {
+    const [hoveredIndex, setHoveredIndex] = useState(null);
+    const [popupPosition, setPopupPosition] = useState({ top: 0, left: 0 });
+
+    const links = [
+        { to: '/', imgSrc: './img/icons/house2.svg', alt: 'home', description: 'Home' },
+        { to: '/contest', imgSrc: './img/icons/trophy2.svg', alt: 'contest', description: 'Contest' },
+        { to: '/add', imgSrc: './img/icons/add3.svg', alt: 'add', description: 'Add' },
+        { to: '/trending', imgSrc: './img/icons/fire2.svg', alt: 'trending', description: 'Trending' },
+        { to: '/profil', imgSrc: './img/icons/user3.svg', alt: 'user', description: 'Profile' },
+    ];
+
+    const handleMouseEnter = (index) => {
+        const icon = document.getElementById(`nav-icon-${index}`);
+        const rect = icon.getBoundingClientRect();
+        setPopupPosition({
+            top: rect.top + window.scrollY + rect.height / 2,
+            left: rect.right + 10,
+        });
+        setHoveredIndex(index);
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredIndex(null);
+    };
+
     return (
         <div className="left-nav-container">
             <div className="icons">
                 <div className="icons-bis">
-                    <NavLink to='/' className={({ isActive }) => (isActive ? "active-left-nav" : "")}>
-                        <img src="./img/icons/house2.svg" alt="home"/>
-                    </NavLink>
-                    <br/>
-                    <NavLink to='/contest' className={({ isActive }) => (isActive ? "active-left-nav" : "")}>
-                        <img src="./img/icons/trophy2.svg" alt="contest"/>
-                    </NavLink>
-                    <br/>
-                    <NavLink to='/add' className={({ isActive }) => (isActive ? "active-left-nav" : "")}>
-                        <img src="./img/icons/add3.svg" alt="add"/>
-                    </NavLink>
-                    <br/>
-                    <NavLink to='/trending' className={({ isActive }) => (isActive ? "active-left-nav" : "")}>
-                        <img src="./img/icons/fire2.svg" alt="trending"/>
-                    </NavLink>
-                    <br/>
-                    <NavLink to='/profil' className={({ isActive }) => (isActive ? "active-left-nav" : "")}>
-                        <img src="./img/icons/user3.svg" alt="user"/>
-                    </NavLink>
+                    {links.map((link, index) => (
+                        <NavLink
+                            key={index}
+                            to={link.to}
+                            className={({ isActive }) => (isActive ? "active-left-nav" : "")}
+                            onMouseEnter={() => handleMouseEnter(index)}
+                            onMouseLeave={handleMouseLeave}
+                        >
+                            <img id={`nav-icon-${index}`} src={link.imgSrc} alt={link.alt} />
+                        </NavLink>
+                    ))}
                 </div>
+
+                {hoveredIndex !== null && (
+                    <div
+                        className="nav-popup-container"
+                        style={{ top: `${popupPosition.top}px`, left: `${popupPosition.left}px` }}
+                    >
+                        {links[hoveredIndex].description}
+                    </div>
+                )}
             </div>
         </div>
     );
