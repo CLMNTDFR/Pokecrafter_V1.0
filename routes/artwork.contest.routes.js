8aaf2ce261401b71f719cc7a6010acc4724de778
TanguyMoreau99 <tanguymoreau.aes@gmail.com>
2024-10-01 14:30:47 +0200

    add contest feature, 2 new models and their routes

---
+++ routes/artwork.contest.routes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const artworkContestController = require('../controllers/artwork.contest.controller');
+const fileUpload = require('express-fileupload'); // Require express-fileupload
+const path = require('path');
+
+// Routes pour les ArtworkContest
+router.post('/', artworkContestController.createArtworkContest);
+router.get('/:id', artworkContestController.getArtworkContestById);
+// Route pour obtenir tous les artworks d'un contest par l'ID du contest
+router.get('/contest/:contestID', artworkContestController.getArtworksByContestId);
+router.put('/:id', artworkContestController.updateArtworkContest);
+router.delete('/:id', artworkContestController.deleteArtworkContest);
+
+// like and dislike
+
+router.patch('/like-artwork-contest/:id', artworkContestController.likeArtworkContest);
+router.patch('/unlike-artwork-contest/:id', artworkContestController.unlikeArtworkContest);
+
+// Comments
+router.patch('/comment-artwork-contest/:id', artworkContestController.commentArtworkContest);
+router.patch('/edit-comment-artwork-contest/:id', artworkContestController.editCommentArtworkContest);
+router.patch('/delete-comment-artwork-contest/:id', artworkContestController.deleteCommentArtworkContest);
+
+
+
+module.exports = router;
